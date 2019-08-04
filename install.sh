a72406ebdc0b9c784e72e7c4b1087d0bfc1ffe59
SuperSonic <supersonic@livemail.tw>
2019-08-04 22:32:53 +0800

    Fix Install Shell Script

--- install.sh
+++ install.sh
@@ -2,12 +2,7 @@
 # License: GPL 3.0
 # (c) 2019 Star Inc.
 
-if [ -f /etc/os-release ]; then
-    OS=$NAME
-    VER=$VERSION_ID
-fi
-
-if [ "$OS" == "Ubuntu" ] && [ $VER -ge 16 ]; then
+if [ $(lsb_release -si) == "Ubuntu" ]; then
     apt-get install -y python3 gir1.2-webkit-3.0 python-gi
 else
     echo "Now, it was only support/tested on \"the Ubuntu for AnLinux\""
