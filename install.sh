42868d74fb0f093198a141b21cfdabdca771ced1
SuperSonic <supersonic@livemail.tw>
2019-08-05 17:37:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # (c) 2019 Star Inc.
 
 if [ $(lsb_release -si) == "Ubuntu" ]; then
-    apt-get install -y python3 gir1.2-webkit-3.0 python-gi
+    apt-get install -y python3 gir1.2-webkit-3.0 python-gi fonts-droid-fallback
 else
     echo "Now, it was only support/tested on \"the Ubuntu for AnLinux\""
     echo "But you still can install the browser on other Linux distribution without the shell script."
